85600dfe33d3dada2d8a28a0d53e485657dd4dde
David Ivins <davidivins@gmail.com>
2011-05-22 15:53:22 -0400

    first activity
    Signed-off-by: David Ivins <davidivins@gmail.com>

---
+++ src/com/davidivins/checkin4menfc/CheckIn4MeNFCAddOn.java
@@ -0,0 +1,38 @@
+//*****************************************************************************
+//    This file is part of CheckIn4Me.  Copyright © 2010  David Ivins
+//
+//    CheckIn4Me is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    CheckIn4Me is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with CheckIn4Me.  If not, see <http://www.gnu.org/licenses/>.
+//*****************************************************************************
+package com.davidivins.checkin4menfc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * CheckIn4MeNFCAddOn **DEBUG**
+ * 
+ * @author david ivins
+ */
+public class CheckIn4MeNFCAddOn extends Activity
+{
+	/**
+	 * onCreate
+	 * 
+	 * @param Bundle saved_instance_state
+	 */
+	@Override
+	public void onCreate(Bundle saved_instance_state)
+	{
+	}
+}
